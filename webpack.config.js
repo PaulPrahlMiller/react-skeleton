0f4c2b28d26200157bb1ddbe7dcb052b28976306
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-11-26 01:53:00 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    hot: true,
+    open: true,
+    port: 3000,
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+};
